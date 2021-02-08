43f97e947b2f2147654828f86d909c71982231cb
zhangdi <id.zhang@i9i8.com>
2021-02-08 11:47:11 +0800

    kong simple use

---
+++ spring-boot-kong/src/main/java/org/study/Application.java
@@ -0,0 +1,17 @@
+package org.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dizhang
+ * @date 2021-02-07
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+}
